5ccc56cf2cd48bdcb1574eebab5ed2ee23753928
Vitruvius <vitrvvivs.architectus@gmail.com>
2023-10-12 18:48:49 -0400

    s/Connect()/ConnectNoVerify()/
    to override incorrect oasis-sdk network config chainContext

--- faucet-backend/bank.go
+++ faucet-backend/bank.go
@@ -34,7 +34,7 @@ func (svc *Service) paratimeName(paratimeId string) string {
 type FundRequest struct {
 	ParaTime *config.ParaTime
 	Account  *types.Address
-    EthAccount *ethCommon.Address
+	EthAccount *ethCommon.Address
 
 	ConsensusAmount *types.Quantity
 	ParaTimeAmount  *types.BaseUnits
@@ -52,12 +52,20 @@ func (svc *Service) BankWorker() {
 	)
 	for {
 		svc.log.Printf("bank: attempting to connect to gRPC endpoint")
-		if conn, err = connection.Connect(ctx, svc.network); err != nil {
+        // XXX: Revert to Connect() when oasis-sdk updates to be compatible with oasis-core v23
+		if conn, err = connection.ConnectNoVerify(ctx, svc.network); err != nil {
 			svc.log.Printf("bank: failed to connect to node: %v", err)
 			time.Sleep(15 * time.Second)
 			continue
 		}
 		break
+	}
+
+	cs := conn.Consensus()
+	chainContext, err := cs.GetChainContext(ctx)
+	svc.network.ChainContext = chainContext
+	if err != nil {
+		svc.log.Printf("bank: failed to retrieve remote node's chain context: %s", err)
 	}
 
 	// Refill the allowances.
